16cd3d050113fd896eaced0ceee5da2250429b06
gustavoDev02 <guga19italo@gmail.com>
2024-05-30 02:38:01 -0300

    Arrumando bug pós merge da tela inicial

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,29 @@
 import { Link } from "react-router-dom"
 import './header.css';
 import Logo from "../img/Logo.png"
+import Harmonia from "./harmonia";
 
 
 export default function Cabecalho(){
     return(
         <header className="header">
+         
+          <nav className="navigation">
           <div className="logo">
             <img src={Logo} alt="Logo Doracorde"/> 
           </div>
-          <nav className="navigation">
             <a href="/home">Home</a>
             <a href="/instrumentos">Instrumentos</a>
             <a href="/planos">Planos</a>
             <a href="/depoimentos">Depoimentos</a>
             <a href="/sobre">Sobre</a>
             {/* <Link to='/blog'> <a >Blog</a> </Link> */}
+            <div className="buttons">
+              <button className="login">Entrar</button>
+              <button className="start-now">Comece Agora</button>
+            </div>
           </nav>
-          <div className="buttons">
-            <button className="login">Entrar</button>
-            <button className="start-now">Comece Agora</button>
-          </div>
+          <Harmonia/>
         </header>
     )
 }
