498df563e9db58ef864bfdec4cd5237fa4dd05ac
gustavoDev02 <guga19italo@gmail.com>
2024-07-01 18:33:31 -0300

    fazendo responsividade dos exercicios

--- src/components/Exercicio-Acorde/ExercicioAcorde.js
+++ src/components/Exercicio-Acorde/ExercicioAcorde.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import './ExercicioAcorde.css';
 import acerto from '../../assets/audio/correctSound.mp3';
 
+
 export default function ExercicioAcorde(props) {
 
     const [disabledButtons, setDisabledButtons] = useState([]);
