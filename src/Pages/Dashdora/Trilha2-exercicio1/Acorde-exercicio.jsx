7cb679c8fb072ef4c9c7eaebc6ee2dd75b76e765
gustavoDev02 <guga19italo@gmail.com>
2024-07-01 20:43:51 -0300

    fazendo ajuste na responsividade apos tablatura

--- src/Pages/Dashdora/Trilha2-exercicio1/Acorde-exercicio.jsx
+++ src/Pages/Dashdora/Trilha2-exercicio1/Acorde-exercicio.jsx
@@ -3,13 +3,19 @@ import ExercicioAcorde from "../../../components/Exercicio-Acorde/ExercicioAcord
 import acordeAudio from '../../../assets/audio/acorde1-diminutos.mp3';
 import { useTranslation } from 'react-i18next';
 import '../../../i18n'
-
+import Logo from '../../../assets/img/Logo.png';
+import { Link } from 'react-router-dom';
 
 export default function AcordeExercicio() {
     const { t } = useTranslation();
 
     return (
         <>
+            <div className="header-dashdora">
+                <Link to='/instrumentos'>
+                    <img src={Logo} alt="Doracorde Logo" className="logo" />
+                </Link>
+            </div>
             <ExercicioAcorde
                 titulo={t("Que acorde está sendo tocado ?")}
                 nomesOpcoes={[
